9eb45144f8145bdc298818ad7a35740d0accaf6e
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-10 12:37:42 +0100

    solve remove duplicate test

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,11 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!newArr.includes(arr[i])) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr
+}
 
 module.exports = removeDuplicates;
