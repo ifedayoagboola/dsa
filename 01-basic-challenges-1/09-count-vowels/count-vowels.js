fba6fde8dd963f99a0d6ff4745ef94be43cb9fa8
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-10 12:28:37 +0100

    solve count vowels test

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,18 @@
-function countVowels() {}
+function countVowels(str) {
+  const fmt = str.toLowerCase();
+  let vowels = 0;
+  for (let i = 0; i < fmt.length; i++) {
+    if (
+      fmt[i] === "a" ||
+      fmt[i] === "e" ||
+      fmt[i] === "i" ||
+      fmt[i] === "o" ||
+      fmt[i] === "u"
+    ) {
+      vowels++;
+    }
+  }
+  return vowels;
+}
 
 module.exports = countVowels;
