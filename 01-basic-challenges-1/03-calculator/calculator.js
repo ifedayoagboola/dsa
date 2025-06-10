c11133e6f52af97042980ec0e6cac1100a4c610d
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-10 08:50:00 +0100

    solve basic challenges1 1- 7

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,18 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+     return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+    case "%":
+      return num1 % num2;
+    default:
+      new Error("Invalid Operator");
+  }
+}
 
 module.exports = calculator;
