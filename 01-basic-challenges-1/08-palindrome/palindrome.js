f493180b229e1223bbf445b395b7e3e0fc957307
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-10 12:03:24 +0100

    solve Palindrome test

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,23 @@
-function isPalindrome() {}
+function isPalindrome(input) {
+  let formattedInput = "";
+  let reversedWord = "";
+  const char = input.toLowerCase();
+  for (let i = 0; i < char.length; i++) {
+    if (
+      (char[i]?.charCodeAt(0) >= 48 && char[i]?.charCodeAt(0) <= 57) ||
+      (char[i]?.charCodeAt(0) >= 97 && char[i]?.charCodeAt(0) <= 122)
+    ) {
+      formattedInput += char[i];
+    }
+  }
+
+  for (let i = formattedInput.length - 1; i >= 0; i--) {
+    reversedWord += formattedInput[i];
+  }
+
+  const palindrome = reversedWord === formattedInput;
+  
+  return palindrome;
+}
 
 module.exports = isPalindrome;
