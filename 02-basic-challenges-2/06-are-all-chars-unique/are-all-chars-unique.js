e70169545f667e7e3f9d0d8390b058daca558819
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-16 13:56:32 +0100

    unique characters

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,15 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  let charCount = {}
+  
+  for(let i =0; i<str.length; i++){
+    const char = str[i]
+
+    if(charCount[char]){
+        return false
+    }
+    charCount[char] = true
+  }
+  return true
+}
 
 module.exports = areAllCharactersUnique;
