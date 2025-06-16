61b9a793e173c3f9f7c3df97b50e6f5dc451bddd
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-16 08:05:19 +0100

    array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,12 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+let int = []
+for(let i = 0; i<arr1.length; i++){
+    if(arr2.includes(arr1[i])){
+        int.push(arr1[i])
+    }
+}
+
+return int
+}
 
 module.exports = arrayIntersection;
