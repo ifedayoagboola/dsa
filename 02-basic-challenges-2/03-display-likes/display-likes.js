42bd705563bf08192ac4ac488f9c15717d7e0496
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-16 08:18:43 +0100

    display likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,19 @@
-function displayLikes() {}
+function displayLikes(names) {
+  let str = "";
+
+  if (names.length === 0) {
+    str = "no one likes this";
+  } else if (names.length === 1) {
+    str = `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    str = `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    str = `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else if (names.length > 3) {
+    str = `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+
+  return str;
+}
 
 module.exports = displayLikes;
