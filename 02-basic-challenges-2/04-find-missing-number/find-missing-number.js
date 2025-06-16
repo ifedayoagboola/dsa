ac08dfbdf379f67e7c69968e3e7be5215fcd2ebf
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-16 09:07:44 +0100

    find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,16 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+    if(arr.length === 0){
+        return
+    }
+  let n = arr.length + 1;
+
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = arr.reduce((sum, num) => {
+    return sum + num;
+  }, 0);
+
+  return expectedSum - actualSum
+
+}
 
 module.exports = findMissingNumber;
