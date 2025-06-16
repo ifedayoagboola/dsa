79f9d7aa4ecd2b30754daf4f5447194ee7ad392d
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-16 09:44:02 +0100

    Find missing letter.

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,12 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  let alp = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let startIndex = alp.indexOf(arr[0]);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== alp[startIndex + i]) {
+      return alp[startIndex + i];
+    }
+  }
+}
 
 module.exports = findMissingLetter;
