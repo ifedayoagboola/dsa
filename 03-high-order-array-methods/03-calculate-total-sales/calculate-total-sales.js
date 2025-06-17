9b80dd32bdcf7fe99965d7dba7f78e571d419620
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-17 17:04:28 +0100

    calculate total sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,11 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(arr, tax) {
+  const sales =
+    (arr
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, item) => prev + item)
+    )
+
+    return sales + sales * tax/100
+}
 
 module.exports = calculateTotalSalesWithTax;
