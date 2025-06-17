e9c225d2e829669e35e258350ca23a5bd9317fef
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-17 18:25:01 +0100

    highest scoring word

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,25 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  const fmt = str.toLowerCase().split(" ");
+  const alp = "abcdefghijklmnopqrstuvwxyz";
+  let highestScore = 0;
+  let highestIndex = 0
+  let score = 0;
+
+  fmt.map((word, i) => {
+    for (let i = 0; i < word.length; i++) {
+      score += alp.indexOf(word[i]) + 1;
+    }
+
+    if(score > highestScore){
+        highestScore = score
+        highestIndex = i
+    }
+
+
+  });
+
+  return fmt[highestIndex]
+
+}
 
 module.exports = highestScoringWord;
