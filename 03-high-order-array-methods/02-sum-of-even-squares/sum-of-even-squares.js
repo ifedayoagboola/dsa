743c3c7be8e19ab1f0f0ce17545e1381b1cc6209
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-17 16:48:50 +0100

    sum of even square

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,11 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+  let sum = 0;
+  arr.map((i) => {
+    if (i % 2 === 0) {
+      sum += i ** 2;
+    }
+  });
+  return sum
+}
 
 module.exports = sumOfEvenSquares;
