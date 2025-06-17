08b49c6c6b964e1fca1429f187f2c020ab0e94ac
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-17 15:00:57 +0100

    format phone number

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,10 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+  const fmt = arr.join("");
+
+  const prefix = fmt.slice(0, 3);
+  const mid = fmt.slice(3, 6);
+  const suffix = fmt.slice(6);
+  return `(${prefix}) ${mid}-${suffix}`;
+}
 
 module.exports = formatPhoneNumber;
