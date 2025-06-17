74d2fbbf1485ac82bafc7bc8448b9aa245aa3fa7
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-17 13:39:58 +0100

    dice game

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,23 @@
-function diceGameSimulation() {}
+function diceGameSimulation(numSimulations) {
+  let results = [];
+
+  for (let i = 0; i < numSimulations; i++) {
+    const dice1 = Math.floor(Math.random() * 6) + 1;
+    const dice2 = Math.floor(Math.random() * 6) + 1;
+    const sum = dice1 + dice2;
+    let result = ''
+
+    if (sum === 7 || sum === 11) {
+      result = "win";
+    } else if (sum === 12 || sum === 3 || sum === 2) {
+      result = "lose";
+    } else {
+      result = "roll again";
+    }
+
+    results.push({dice1, dice2, sum, result})
+  }
+  return results;
+}
 
 module.exports = diceGameSimulation;
