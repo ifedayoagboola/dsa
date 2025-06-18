af27c27fa0b9dc0b1c350b9f95d1386275880c44
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-18 13:55:15 +0100

    valid ipv4

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,22 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+  const arr = str.split(".");
+
+  if (arr.length !== 4) {
+    return false
+  }
+
+  return arr.every((i) => {
+    const octet = Number(i);
+
+    if (octet >= 0 && octet <= 255 && octet.toString() === i) {
+      return true;
+    }
+
+  });
+
+};
+
+//This is a tricky one. you should use the .every() method because we are only trying to return a boolean.
+//If we use the .map() method, an array will be return which will fail the test.
 
 module.exports = isValidIPv4;
