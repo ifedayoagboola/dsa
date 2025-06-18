42c204d281043dc01a6545b0444ca2a2aaaca9c5
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-18 19:49:19 +0100

    analyze car mileage

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,23 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(arr) {
+  let highestMileage = 0;
+  let highestMileageCar = {};
+  let lowestMileageCar = {};
+
+  const totalMileage = arr.reduce((acc, car) => {
+    if (car.mileage > highestMileage) {
+      highestMileage = car.mileage;
+      highestMileageCar = car;
+    }
+
+    if (arr[0].mileage > car.mileage) {
+      lowestMileageCar = car;
+    }
+    return acc + car.mileage;
+  }, 0);
+
+  const averageMileage = totalMileage / arr.length;
+
+  return { averageMileage, highestMileageCar, lowestMileageCar, totalMileage };
+}
 
 module.exports = analyzeCarMileage;
