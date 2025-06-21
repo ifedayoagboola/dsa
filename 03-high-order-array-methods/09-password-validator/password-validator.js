e5bb2d178c3335cd8151f32237038fa757f6f938
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-21 13:04:03 +0100

    password validator

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,14 @@
-function validatePassword() {}
+function validatePassword(str) {
+  const fmt = str.split("");
+  const hasUpperCase = fmt.some((char) => {
+    return char === char.toUpperCase() && char !== char.toLowerCase();
+  });
+  const hasLowerCase = fmt.some((char) => {
+    return char === char.toLowerCase() && char !== char.toUpperCase();
+  });
+  const hasInteger = fmt.some((char) => !isNaN(parseInt(char, 10)));
+
+return hasUpperCase && hasLowerCase && hasInteger && str.length >= 8
+}
 
 module.exports = validatePassword;
