ab8accfa76b63a8fafc58027edbfd09a41229a36
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-25 17:43:34 +0100

    base, power and exponent

--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,3 +1,19 @@
-function power() {}
+function power(base, exp) {
+    if(exp ===0){
+        return 1
+    }
+    return base * power(base, exp-1)
+}
+
+// function power(base, exp) {
+//     let i = 0
+
+//     if(i === exp){
+//         return 1
+//     }else{
+//         i++
+//     }
+//     return base * power(base, exp-1)
+// }
 
 module.exports = power;
