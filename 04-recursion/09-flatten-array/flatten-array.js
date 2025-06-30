20cca7ff88145156de150f5f9f03b82dcd4e9840
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-30 11:23:22 +0100

    recursion- flatten array

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,14 @@
-function flattenArray() {}
+function flattenArray(arr) {
+  let flat = [];
+
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      flat = flat.concat(flattenArray(item));
+    } else {
+      flat.push(item);
+    }
+  }
+  return flat;
+}
 
 module.exports = flattenArray;
