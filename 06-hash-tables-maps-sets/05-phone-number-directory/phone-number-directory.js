fc4cd8daaa063f42efaf118700f4fbb46185b2e8
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-07-03 22:06:30 +0100

    phone number directory with Map()

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,10 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(phoneNumbers) {
+  const directory = new Map();
+  for (const contact of phoneNumbers) {
+    const [name, phone] = contact.split(":");
+    directory.set(name, phone);
+  }
+  return directory;
+}
 
 module.exports = phoneNumberDirectory;
