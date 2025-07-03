9984e440dd14b1db1251b9551c663fee93b09c23
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-07-03 21:49:53 +0100

    solve word frequency counter with Map()

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,20 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let maxSum = -Infinity;
+
+  for (let i = 0; i <= arr.length - k; i++) {
+    let currentSum = 0;
+
+    for (let j = i; j < i + k; j++) {
+      currentSum += arr[j];
+    }
+
+    //  maxSum = Math.max(maxSum, currentSum);
+    if (currentSum > maxSum) {
+      maxSum = currentSum;
+    }
+  }
+
+  return maxSum;
+}
 
 module.exports = maxSubarraySum;
