9b18213c1064c10ce3250d3489bb0ee15675cf5a
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-07-17 12:26:31 +0100

    anagram grouping

--- 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,3 +1,16 @@
-function anagramGrouping() {}
+function anagramGrouping(arr) {
+  const anagram = new Map();
+
+  for (const word of arr) {
+    const sortedWord = word.split("").sort().join("");
+    if (anagram.has(sortedWord)) {
+      anagram.get(sortedWord).push(word);
+    } else {
+      anagram.set(sortedWord, [word]);
+    }
+  }
+
+  return Array.from(anagram.values());
+}
 
 module.exports = anagramGrouping;
