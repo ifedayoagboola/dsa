f2dea0f4a2a90fa63767a5defb8438f09215b31e
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-07-18 23:13:58 +0100

    daily push

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,14 @@
-function twoSum() {}
+function twoSum(arr, target) {
+const set = new Set()
+
+    for(let i = 0; i<arr.length; i++){
+  const complement = target - arr[i]
+
+  if(set.has(complement)){
+
+  }
+  set.add(indexOf(complement), i)
+    }
+}
 
 module.exports = twoSum;
