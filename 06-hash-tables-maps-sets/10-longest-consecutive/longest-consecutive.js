1f7505481df35e864ce96be6758f4c3a349d810a
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-08-05 10:32:53 +0100

    longest consecutive sequence

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,21 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(arr) {
+  const numSet = new Set(arr);
+  let longestSeq = 0;
+
+  for (num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentSeq = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentSeq++;
+        currentNum++;
+      }
+      longestSeq = Math.max(longestSeq, currentSeq);
+    }
+  }
+
+  return longestSeq;
+}
 
 module.exports = longestConsecutiveSequence;
